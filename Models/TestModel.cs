200f11c60a7812785627db2898d91fc9fd8d1bee
ronny ruiz <roniruiz23@hotmail.com>
2024-08-15 23:12:48 -0600

    prueba

---
+++ Models/TestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaVeterinaria.Models
+{
+    public class TestModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
